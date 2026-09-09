af49c00fa47df98f43d0bfad1f584267e1a4bc78
WSUGlobalCampusMediaDesign <bryanvarner@wsu.edu>
2026-09-09 10:52:29 -0700

    Add files via upload

---
+++ BIO 333 Study Session_20260909/story_content/triggers.js
@@ -0,0 +1,173 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5sF0SDMVj2D":
+        Script1();
+        break;
+      case "6EvN3WzswBf":
+        Script2();
+        break;
+      case "6iI92qzamm0":
+        Script3();
+        break;
+      case "5YfQpDhrnIX":
+        Script4();
+        break;
+      case "5YJNzljApfm":
+        Script5();
+        break;
+      case "5kQmiPXBZoC":
+        Script6();
+        break;
+      case "6jIVhDqWdZa":
+        Script7();
+        break;
+      case "62ODlYQYLAT":
+        Script8();
+        break;
+      case "5dC8PU62IMp":
+        Script9();
+        break;
+      case "5Y847gEMGjW":
+        Script10();
+        break;
+      case "5zRsdisLxSX":
+        Script11();
+        break;
+      case "6jpV44DKDda":
+        Script12();
+        break;
+      case "6qglNCrT5qE":
+        Script13();
+        break;
+      case "5qMlK8zxkzY":
+        Script14();
+        break;
+      case "6PiAApjhAng":
+        Script15();
+        break;
+      case "6GzqIFTAYdf":
+        Script16();
+        break;
+      case "6M877HXtzUc":
+        Script17();
+        break;
+      case "6q8uT40mSbQ":
+        Script18();
+        break;
+      case "6pQuMPQSZlD":
+        Script19();
+        break;
+      case "5drKyQpPH1x":
+        Script20();
+        break;
+      case "5jJfq4KVKVv":
+        Script21();
+        break;
+      case "666dglRzJgk":
+        Script22();
+        break;
+      case "5XbYrRzXn0h":
+        Script23();
+        break;
+      case "5oNn2v2RIgP":
+        Script24();
+        break;
+      case "6YXtAkOO0Gy":
+        Script25();
+        break;
+      case "6ln2LsOy95S":
+        Script26();
+        break;
+      case "5cmswfBSCdh":
+        Script27();
+        break;
+      case "65MLdr0v5rJ":
+        Script28();
+        break;
+      case "5rjnzvJbejL":
+        Script29();
+        break;
+      case "5wdBy59jyXR":
+        Script30();
+        break;
+      case "5b04zlAXA5w":
+        Script31();
+        break;
+      case "6CE0eBdoO8W":
+        Script32();
+        break;
+      case "6lepOkQDmHc":
+        Script33();
+        break;
+      case "6fcS22iDr39":
+        Script34();
+        break;
+      case "6b6DL4ua8jw":
+        Script35();
+        break;
+      case "68Onen3JEBF":
+        Script36();
+        break;
+      case "6QA4gWE0jku":
+        Script37();
+        break;
+      case "6LfVKAC8L3M":
+        Script38();
+        break;
+      case "69KzXuy9VNR":
+        Script39();
+        break;
+      case "60CN2uy6eBP":
+        Script40();
+        break;
+      case "6QyWichEGMt":
+        Script41();
+        break;
+      case "6ZIE854bn7J":
+        Script42();
+        break;
+      case "5tuNJBPndm6":
+        Script43();
+        break;
+      case "6fvxqUyjEe3":
+        Script44();
+        break;
+      case "6XEHkkGnL8M":
+        Script45();
+        break;
+      case "6gSezFSFDUP":
+        Script46();
+        break;
+      case "6NIMngED1yh":
+        Script47();
+        break;
+      case "6Ibc7fbPilg":
+        Script48();
+        break;
+      case "5y6JYgOZ4ns":
+        Script49();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+var getKeyDown = player.getKeyDown;
+var keydown = player.keydown;
+var keyup = player.keyup;
+};

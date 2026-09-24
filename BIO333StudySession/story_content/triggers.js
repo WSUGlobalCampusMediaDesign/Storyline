37eced3bb5272944521d89ad2621f3f1fe3ea9ed
WSUGlobalCampusMediaDesign <bryanvarner@wsu.edu>
2026-09-24 07:00:53 -0700

    Add files via upload

---
+++ BIO333StudySession/story_content/triggers.js
@@ -0,0 +1,281 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6eEAU9VuIm9":
+        Script1();
+        break;
+      case "6APtdCE6tYH":
+        Script2();
+        break;
+      case "6XhhHHxUnOx":
+        Script3();
+        break;
+      case "5nqdkyuAPeq":
+        Script4();
+        break;
+      case "62Cf7jqR3ju":
+        Script5();
+        break;
+      case "6Vxx7MqSLqg":
+        Script6();
+        break;
+      case "5u9d8c9mbBH":
+        Script7();
+        break;
+      case "6AuGZZiDvur":
+        Script8();
+        break;
+      case "5eU8CaitxUO":
+        Script9();
+        break;
+      case "6JPVqM8m2HG":
+        Script10();
+        break;
+      case "6qPT6MaS2aa":
+        Script11();
+        break;
+      case "65drtPfjXPM":
+        Script12();
+        break;
+      case "6r2UV0zzSbs":
+        Script13();
+        break;
+      case "6n67NgHK8QY":
+        Script14();
+        break;
+      case "607EyDEcbvb":
+        Script15();
+        break;
+      case "5iE5iGAkCEz":
+        Script16();
+        break;
+      case "6kOuuVa8rAa":
+        Script17();
+        break;
+      case "6QISqVCFTu0":
+        Script18();
+        break;
+      case "6ffo4RxNJrY":
+        Script19();
+        break;
+      case "6hl0VERdH3w":
+        Script20();
+        break;
+      case "6XjxdHuDsql":
+        Script21();
+        break;
+      case "5ldkSunAHbD":
+        Script22();
+        break;
+      case "6W2qXQPOswx":
+        Script23();
+        break;
+      case "5ia6QBOM9vv":
+        Script24();
+        break;
+      case "5i0sHyTAL99":
+        Script25();
+        break;
+      case "6eLXrtd3jOv":
+        Script26();
+        break;
+      case "6kMkpKeoDUw":
+        Script27();
+        break;
+      case "6KzlSLBGwjp":
+        Script28();
+        break;
+      case "5lG4mOKe8Ve":
+        Script29();
+        break;
+      case "6CjBvktkCVm":
+        Script30();
+        break;
+      case "6DAniGpZIev":
+        Script31();
+        break;
+      case "5tkMRz5X2F7":
+        Script32();
+        break;
+      case "6WqptA3EzNY":
+        Script33();
+        break;
+      case "5bFfnDcDH0p":
+        Script34();
+        break;
+      case "5rzhIdtiTbE":
+        Script35();
+        break;
+      case "6WE6S1vG0tc":
+        Script36();
+        break;
+      case "6IORIxKoEpx":
+        Script37();
+        break;
+      case "6gdHNBun7eh":
+        Script38();
+        break;
+      case "5aaju9pPXIR":
+        Script39();
+        break;
+      case "6VT3kXnBy7e":
+        Script40();
+        break;
+      case "6m1bmo09NDu":
+        Script41();
+        break;
+      case "6CcV4zxfWaL":
+        Script42();
+        break;
+      case "6MvrlyB7eHN":
+        Script43();
+        break;
+      case "6klI82XYoFb":
+        Script44();
+        break;
+      case "6bjaDTvvgy3":
+        Script45();
+        break;
+      case "62h50YsrMl5":
+        Script46();
+        break;
+      case "64vlFUpuDhf":
+        Script47();
+        break;
+      case "6SJRa1O1iH0":
+        Script48();
+        break;
+      case "61D4DH0s1zz":
+        Script49();
+        break;
+      case "6iJ3iII17Sc":
+        Script50();
+        break;
+      case "5f3rscUP9ne":
+        Script51();
+        break;
+      case "5g5H47vaeYK":
+        Script52();
+        break;
+      case "6dOKOq1GjQW":
+        Script53();
+        break;
+      case "6TKHkSYx7qm":
+        Script54();
+        break;
+      case "67TGit1RUUb":
+        Script55();
+        break;
+      case "5fZC15h3FzG":
+        Script56();
+        break;
+      case "5uPahmfg3Ab":
+        Script57();
+        break;
+      case "6E8Wti4qxbP":
+        Script58();
+        break;
+      case "63qIbqrbDZD":
+        Script59();
+        break;
+      case "6iQeedPf5Yp":
+        Script60();
+        break;
+      case "6KfPEpiPLXq":
+        Script61();
+        break;
+      case "67plZkDKf66":
+        Script62();
+        break;
+      case "6D91WQPbUHe":
+        Script63();
+        break;
+      case "5ia1LEZamuN":
+        Script64();
+        break;
+      case "6eOUABjU3z0":
+        Script65();
+        break;
+      case "6qg23Am61ON":
+        Script66();
+        break;
+      case "6SFSEr0Fwsp":
+        Script67();
+        break;
+      case "5ikqSMEgLff":
+        Script68();
+        break;
+      case "6KcLy4LDwBE":
+        Script69();
+        break;
+      case "5YOPAOgiK0i":
+        Script70();
+        break;
+      case "5dWcYr0xP8N":
+        Script71();
+        break;
+      case "60pfOUrbXfP":
+        Script72();
+        break;
+      case "6IO8h2dmi9r":
+        Script73();
+        break;
+      case "63ztfKLsJ2A":
+        Script74();
+        break;
+      case "5XctOG654Ea":
+        Script75();
+        break;
+      case "6CJZtpU07pV":
+        Script76();
+        break;
+      case "6k4VKI6fCvo":
+        Script77();
+        break;
+      case "6djeQV39als":
+        Script78();
+        break;
+      case "623PE8LRtRS":
+        Script79();
+        break;
+      case "5uwDPy3qpsE":
+        Script80();
+        break;
+      case "6otHJODkVDp":
+        Script81();
+        break;
+      case "67eGo4RUSq3":
+        Script82();
+        break;
+      case "5vxt0H7ShgP":
+        Script83();
+        break;
+      case "6K6PiZPLQ7k":
+        Script84();
+        break;
+      case "6eqtqSM1QkU":
+        Script85();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+var getKeyDown = player.getKeyDown;
+var keydown = player.keydown;
+var keyup = player.keyup;
+};
